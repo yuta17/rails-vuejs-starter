a2a3d732184b394f07c7d507841dc5e5322a0416
Hassan <usgitan@gmail.com>
2019-12-29 15:29:58 +0900

    Renamed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "rails_vuejs_ssr_starter_production"
+  # config.active_job.queue_name_prefix = "rails_vuejs_starter_production"
 
   config.action_mailer.perform_caching = false
 
